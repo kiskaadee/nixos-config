7d8b167304eca8b7caddca7290ecf401b7569695
kiskaadee <fcortesbio@gmail.com>
2026-06-19 00:58:30 -0500

    Add systemd user target for Hyprland graphical session

--- modules/user/graphical.nix
+++ modules/user/graphical.nix
@@ -47,4 +47,14 @@
       ]))
     ];
   };
+
+  systemd.user.targets.hyprland-session = {
+    Unit = {
+      Description = "Hyprland graphical session";
+      Documentation = [ "man:systemd.special(7)" ];
+      BindsTo = [ "graphical-session.target" ];
+      Wants = [ "graphical-session-pre.target" ];
+      After = [ "graphical-session-pre.target" ];
+    };
+  };
 }
