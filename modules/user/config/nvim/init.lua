3496a05674dec37097cf7aba76f6dc0dd0de650f
kiskaadee <fcortesbio@gmail.com>
2026-07-14 08:12:35 -0500

    refactor(nvim): update completion behavior and map Tab for selection
    - Update `completeopt` to `noinsert` to prevent automatic suggestion
      injection.
    - Add `<Tab>` keymap to accept completion selections when the popup menu
      is visible.

--- modules/user/config/nvim/init.lua
+++ modules/user/config/nvim/init.lua
@@ -6,4 +6,14 @@ vim.opt.expandtab = true       -- Convert tabs to spaces
 vim.opt.smartindent = true     -- Intelligent auto-indenting based on file syntax
 vim.opt.wrap = false           -- Disable automatic line wrapping
 vim.opt.termguicolors = true   -- Enable 24-bit RGB terminal colors
-vim.opt.completeopt = { "menu", "menuone", "noselect" } -- Smooth auto-completion behavior
+vim.opt.completeopt = { "menu", "menuone", "noinsert" } -- Smooth auto-completion behavior
+
+-- Accept completion with Tab when the completion menu is visible
+vim.keymap.set('i', '<Tab>', function()
+  if vim.fn.pumvisible() == 1 then
+    return '<C-y>'
+  else
+    return '<Tab>'
+  end
+end, { expr = true, silent = true })
+
