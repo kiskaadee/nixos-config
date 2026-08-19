9114b1f9f3c5d7cfce4bac173d94b0a4c2bc49ff
kiskaadee <fcortesbio@gmail.com>
2026-08-19 18:44:03 -0500

    feat(shell): add cargo binary directory to home session path

--- home.nix
+++ home.nix
@@ -17,6 +17,11 @@
   home.username = "kiskaadee";
   home.homeDirectory = "/home/kiskaadee";
 
+  # Ensure cargo/bin (and other user binaries) are in the system PATH
+  home.sessionPath = [
+    "$HOME/.cargo/bin"
+  ];
+
   # The state version of Home Manager that this configuration is compatible with.
   # Avoid changing this value even if you upgrade NixOS.
   home.stateVersion = "26.05";
